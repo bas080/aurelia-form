ce4590a08c34fa00d7a0d60b8cfec2d8c3da6d80
bas080 <bas080@hotmail.com>
2016-08-30 01:05:00 +0200

    draft

--- src/component/framework/options.js
+++ src/component/framework/options.js
@@ -1,13 +1,32 @@
 import {logger} from '../../aurelia-form';
+import {isPrimitive} from 'isPrimitive';
 
 export class Options {
 
+  constructor(bindingEngine) {
+    this.bindingEngine = bindingEngine;
+  }
+
+  detached() {
+    this.subscription.dispose();
+  }
+
   activate(model) {
-    if (!Array.isArray(model.element.options)) {
-      logger.error('element \'s options should be an array');
+    let options = model.element.options;
+
+    if (isPrimitive(options)) {
+      logger.error('element \'s options should be an array, promise or function');
     }
 
-    this.model = model;
+    this.subscription = bindingEngine.propertyObserver('options', model.element).subscribe(optionsChanged);
+  }
+
+  optionsChanged(options) {
+    if (typeof options === 'function') {
+      model.element.options = model.element.options(model.model);
+    }
+
+    this.model.element.options = model.element.options(this.mode.model);
   }
 
 }
